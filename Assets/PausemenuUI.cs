7d37e431f0c25450b8ed06c57bf25eb0e66efcfc
Sakshi Priyam <sakship25@iitk.ac.in>
2026-03-11 18:22:28 +0530

    pausemenuUI

---
+++ Assets/PausemenuUI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public GameObject optionsPanel;
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume button pressed");
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+}
+
+
