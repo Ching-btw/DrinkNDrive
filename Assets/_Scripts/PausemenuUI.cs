655bca281a24c98700fd3bcca23158e6a343a018
Sakshi Priyam <sakship25@iitk.ac.in>
2026-03-12 23:00:54 +0530

    pausemenuUI changes

--- Assets/_Scripts/PausemenuUI.cs
+++ Assets/_Scripts/PausemenuUI.cs
@@ -9,6 +9,17 @@ public class PauseMenuUI : MonoBehaviour
         optionsPanel.SetActive(false);   
     }
 
+
+    public void OnHover(GameObject button)
+    {
+        button.transform.localScale = Vector3.Lerp(button.transform.localScale, new Vector3(1.25f, 1.25f, 1.25f), Time.deltaTime * 100f);
+    }
+
+    public void OnHover1(GameObject Button)
+    {
+        Button.transform.localScale = Vector3.Lerp(Button.transform.localScale, new Vector3(1f, 1f, 1f), Time.deltaTime * 100f);
+
+    }
     public void ResumeGame()
     {
         Debug.Log("Resume button pressed");
